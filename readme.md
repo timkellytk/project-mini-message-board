3a8269dd416287337e9748ccad80f8eb329d5636
Tim Kelly <tim.kelly@sca.com.au>
2021-02-08 11:31:44 +1100

    Finished the basic message board

---
+++ readme.md
@@ -0,0 +1,5 @@
+# Node - Mini Message Board
+
+> From The Odin Project's [curriculum](https://www.theodinproject.com/courses/nodejs/lessons/mini-message-board)
+
+This project is a very basic message board to learn the fundamentals of Express.
--- routes/index.js
+++ routes/index.js
@@ -19,4 +19,12 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'The Odin Project Message Board', messages });
 });
 
+/* POST new message to home page. */
+router.post('/added-message', function (req, res, next) {
+  const newMessage = { text: req.body.message, user: req.body.author, added: new Date };
+  messages.push(newMessage);
+  res.redirect('/');
+});
+
+
 module.exports = router;
